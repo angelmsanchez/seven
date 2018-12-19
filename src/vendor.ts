43347082c83cb0932e99909df0bd34a0fd2d334d
Angel Munoz <angelmsanchez85@gmail.com>
2018-12-19 10:02:18 +0100

    -add enviornment webpack

---
+++ src/vendor.ts
@@ -0,0 +1,16 @@
+// Angular
+import '@angular/animations';
+import '@angular/cdk';
+import '@angular/common';
+import '@angular/compiler';
+import '@angular/core';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/router';
+import '@angular/material';
+import '@angular/service-worker';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+
+// RxJS
+import 'rxjs';
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,33 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const environment = (process.env.NODE_ENV || 'development').trim();
+const isDev = process.env.NODE_ENV !== 'production';
+console.log('IsDev: ' + isDev);
 
 module.exports = {
-  mode: 'development',
+  mode: environment,
   resolve: {
     extensions: ['.js', '.ts']
   },
   entry: {
+    vendor: './src/vendor.ts',
     polyfills: './src/polyfills.ts',
     main: './src/main.ts'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].[hash].bundle.js',
+    sourceMapFilename: '[file].map',
+    chunkFilename: '[id].[chunkhash].chunk.js',
+
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
   },
   module: {
     rules: [{
@@ -57,17 +71,32 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: 'body'
+      template: 'src/index.html'
     }),
-
     new webpack.ContextReplacementPlugin(
       /\@angular(\\|\/)core(\\|\/)fesm5/,
       path.resolve(__dirname, 'src'), {}
     ),
     new FilterWarningsPlugin({
       exclude: /System.import/
-    })
+    }),
+    new CleanWebpackPlugin('dist', {}),
+    new CopyWebpackPlugin([{
+        from: 'src/assets',
+        to: 'assets'
+      },
+      {
+        from: 'src/manifest.json'
+      },
+    ]),
+    new UglifyJsPlugin({
+      sourceMap: false,
+      parallel: true
+    }),
+    // new ngw.AngularCompilerPlugin({
+    //   tsConfigPath: helpers.root('tsconfig.aot.json'),
+    //   entryModule: helpers.root('src', 'app', 'app.module#AppModule')
+    // })
   ],
   devtool: 'source-map',
   devServer: {
